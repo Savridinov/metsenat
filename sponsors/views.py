8d4a125c00ad7c9f608e4adefebf66c20551eae8
Savridinov <savridinovs123@gmail.com>
2023-03-31 00:15:55 +0600

    test

--- sponsors/views.py
+++ sponsors/views.py
@@ -1,6 +1,6 @@
 import django_filters.rest_framework
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+# from rest_framework.permissions import IsAdminUser
 from .models import Sponsors
 from .serializers import SponsorsSerializer
 # from db.pagination import DefaultPagination
@@ -9,7 +9,7 @@ from .serializers import SponsorsSerializer
 class SponsorsViewSet(ModelViewSet):
     queryset = Sponsors.objects.all()
     serializer_class = SponsorsSerializer
-    permission_classes = (IsAdminUser, )
+    # permission_classes = (IsAdminUser, )
     # pagination_class = [DefaultPagination]
     filter_backends = (django_filters.rest_framework.DjangoFilterBackend, )
     filterset_fields = ('gender', 'status', 'reg_date', 'sponsorship_amount')
